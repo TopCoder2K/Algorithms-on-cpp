25566d98cb130830a586421aec6cb80592edb37c
TopCoder2000 <pchelintsev.sv@phystech.edu>
2019-10-08 22:34:48 +0300

    Modified version

--- 3semester/Aho-Corasick's_algo_1_4.cpp
+++ 3semester/Aho-Corasick's_algo_1_4.cpp
@@ -9,15 +9,18 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <stack>
+#include <memory>
+#include <string_view>
+//#pragma GCC optimize("Ofast,unroll-all-loops")
+//#pragma GCC optimize("-O3")
 
-const int32_t alphabet_size = 26;
+constexpr int32_t alphabet_size = 26;
 
-class SymbolTree {
+class Trie {
 public:
     class Node {
     public:
-        std::vector<Node *> children;
+        std::vector<std::unique_ptr<Node>> children;
         std::vector<Node *> transition;
         std::vector<int32_t> pattern_substr_numbers;
         Node * const parent;
@@ -30,58 +33,44 @@ public:
                 char_to_parent(edge),
                 up(nullptr),
                 suff_link(nullptr) {
-            children.resize(alphabet_size, nullptr);
-            transition.resize(alphabet_size, nullptr);
+            children.resize(alphabet_size);
+            transition.resize(alphabet_size);
         }
         ~Node() = default;
     };
 
-    explicit SymbolTree(std::vector<std::string>& pattern_substrings) {
-        root_ = new Node(root_, 'a');
+    explicit Trie(std::vector<std::basic_string_view<char>>& pattern_substrings) {
+        root_ = std::make_unique<Node>(root_.get(), 'a');
+        state_ = root_.get();
 
         for (int32_t i = 0; i < pattern_substrings.size(); ++i) {
             AddString(pattern_substrings[i], i);
         }
     }
-    ~SymbolTree();
+    ~Trie() = default;
 
-    SymbolTree& operator=(const SymbolTree& other) = delete;
-    SymbolTree& operator=(SymbolTree&& other) = delete;
-    SymbolTree(const SymbolTree& other) = delete;
-    SymbolTree(SymbolTree&& other) = delete;
+    Trie& operator=(const Trie& other) = delete;
+    Trie& operator=(Trie&& other) = delete;
+    Trie(const Trie& other) = delete;
+    Trie(Trie&& other) = delete;
 
     Node* GetSuffLink(Node * const node);
     Node* GetLink(Node * const node, const char symbol_to_parent);
     Node* GetUp(Node * const node);
-    void AddString(std::string& str, const int32_t str_number);
+    void AddString(const std::basic_string_view<char>& str, const int32_t str_number);
+    void NextState(const char sym, std::vector<int32_t>& pattern_positions, std::vector<int32_t>& substr_end_positions);
 
-    Node* GetRoot() { return root_;}
+    Node* GetRoot() { return root_.get();}
 private:
-    Node *root_;
+    std::unique_ptr<Node> root_;
+    Node *state_;
 };
 
-SymbolTree::~SymbolTree() {
-    std::stack<Node*> stack;
-    stack.push(root_);
-
-    while (!stack.empty()) {
-        Node *cur_node = stack.top();
-        stack.pop();
-
-        for (int32_t i = 0; i < alphabet_size; ++i) {
-            if (cur_node->children[i])
-                stack.push(cur_node->children[i]);
-        }
-
-        delete cur_node;
-    }
-}
-
-SymbolTree::Node* SymbolTree::GetSuffLink(SymbolTree::Node * const node) {
+Trie::Node* Trie::GetSuffLink(Trie::Node * const node) {
     if (!node->suff_link) {
         // If node is root child we also make suffix link is root, because empty suffixes are not considered.
-        if (node == root_ || node->parent == root_) {
-            node->suff_link = root_;
+        if (node == root_.get() || node->parent == root_.get()) {
+            node->suff_link = root_.get();
         } else {
             node->suff_link = GetLink(GetSuffLink(node->parent), node->char_to_parent);
         }
@@ -90,14 +79,14 @@ SymbolTree::Node* SymbolTree::GetSuffLink(SymbolTree::Node * const node) {
     return node->suff_link;
 }
 
-SymbolTree::Node* SymbolTree::GetLink(SymbolTree::Node * const node, const char symbol_to_parent) {
+Trie::Node* Trie::GetLink(Trie::Node * const node, const char symbol_to_parent) {
     Node *transition_ptr = node->transition[symbol_to_parent - 'a'];
 
     if (!transition_ptr) {
         if (node->children[symbol_to_parent - 'a']) {
-            transition_ptr = node->children[symbol_to_parent - 'a'];
-        } else if (node == root_) {
-            transition_ptr = root_;
+            transition_ptr = node->children[symbol_to_parent - 'a'].get();
+        } else if (node == root_.get()) {
+            transition_ptr = root_.get();
         } else {
             transition_ptr = GetLink(GetSuffLink(node), symbol_to_parent);
         }
@@ -106,14 +95,14 @@ SymbolTree::Node* SymbolTree::GetLink(SymbolTree::Node * const node, const char 
     return transition_ptr;
 }
 
-SymbolTree::Node* SymbolTree::GetUp(SymbolTree::Node * const node) {
+Trie::Node* Trie::GetUp(Trie::Node * const node) {
     Node *suff_link = GetSuffLink(node);
 
     if (!node->up) {
         if (!suff_link->pattern_substr_numbers.empty()) {
             node->up = suff_link;
-        } else if (suff_link == root_) {
-            node->up = root_;
+        } else if (suff_link == root_.get()) {
+            node->up = root_.get();
         } else {
             node->up = GetUp(suff_link);
         }
@@ -122,15 +111,16 @@ SymbolTree::Node* SymbolTree::GetUp(SymbolTree::Node * const node) {
     return node->up;
 }
 
-void SymbolTree::AddString(std::string& str, const int32_t str_number) {
-    Node *cur_node = root_;
+void Trie::AddString(const std::basic_string_view<char>& str, const int32_t str_number) {
+    Node *cur_node = root_.get();
 
     for (int32_t i = 0; i < str.size(); ++i) {
-        Node *child = cur_node->children[str[i] - 'a'];
+        Node *child = nullptr;
+        if (cur_node) child = cur_node->children[str[i] - 'a'].get();
 
         if (!child) {
-            child = new Node(cur_node, str[i]);
-            cur_node->children[str[i] - 'a'] = child;
+            cur_node->children[str[i] - 'a'] = std::make_unique<Node>(cur_node, str[i]);
+            child = cur_node->children[str[i] - 'a'].get();
         }
 
         cur_node = child;
@@ -139,56 +129,48 @@ void SymbolTree::AddString(std::string& str, const int32_t str_number) {
     cur_node->pattern_substr_numbers.push_back(str_number);
 }
 
-//Aho-Corasick's algorithm.
-void FindAllEntries (
-        SymbolTree& bor,
+void Trie::NextState(
+        const char sym,
         std::vector<int32_t>& pattern_positions,
         std::vector<int32_t>& substr_end_positions
-) {
+        ) {
+    state_ = GetLink(state_, sym);
+    Trie::Node *up = state_;
+
+    while (up != root_.get()) {
+        for (int32_t i = 0; i < up->pattern_substr_numbers.size(); ++i) {
+            // Plus 1 because pattern substrings end positions are one more then their position in the pattern.
+            int32_t pattern_pos = pattern_positions.size() - 1 - substr_end_positions[up->pattern_substr_numbers[i]] + 1;
+            if (pattern_pos >= 0)
+                ++pattern_positions[pattern_pos];
+        }
+
+        up = GetUp(up);
+    }
+}
+
+//Aho-Corasick's algorithm.
+std::vector<int32_t> FindAllEntries (Trie& trie, std::vector<int32_t>& substr_end_positions) {
+    std::vector<int32_t> pattern_positions;
     char sym = 0;
-    int32_t cur_pos_in_text = -1;
-    SymbolTree::Node *root = bor.GetRoot();
-    SymbolTree::Node *cur_node = root;
 
-    /*std::istreambuf_iterator<char> start(std::cin);
-    std::istreambuf_iterator<char> end;
-    ++start; // Skip ' ' or '\n' after pattern.
-
-    while (sym != '\n' && start != end) {
-        sym = *start;
-        ++start;
-
-        if (sym < 'a') break; // New line.
-
-        ++cur_pos_in_text;*/
     while (std::cin >> sym) {
-        ++cur_pos_in_text;
         if (sym == '_')
             break;
 
         pattern_positions.push_back(0);
 
-        cur_node = bor.GetLink(cur_node, sym);
-        SymbolTree::Node *up = cur_node;
-
-        while (up != root) {
-            for (int32_t i = 0; i < up->pattern_substr_numbers.size(); ++i) {
-                // Plus 1 because pattern substrings end positions are one more then their position in the pattern.
-                int32_t pattern_pos = cur_pos_in_text - substr_end_positions.at(up->pattern_substr_numbers.at(i)) + 1;
-                if (pattern_pos >= 0 && pattern_pos < pattern_positions.size())
-                    ++pattern_positions.at(pattern_pos);
-            }
-
-            up = bor.GetUp(up);
-        }
+        trie.NextState(sym, pattern_positions, substr_end_positions);
     }
+
+    return pattern_positions;
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::vector<std::string> pattern_substrings;
+    std::vector<std::basic_string_view<char>> pattern_substrings;
     std::vector<int32_t> substr_end_position;
 
     char sym = 0;
@@ -197,32 +179,32 @@ int main() {
 
     std::cin >> pattern;
 
-    while (counter < pattern.size()) {
+    while (counter <= pattern.size()) {
         cur_str.clear();
         sym = pattern[counter];
         ++counter;
 
-        while (sym != '?' && counter < pattern.size()) {
+        while (sym != '?' && counter <= pattern.size()) {
             cur_str += sym;
             sym = pattern[counter];
             ++counter;
         }
 
         if (!cur_str.empty()) {
-            pattern_substrings.push_back(cur_str);
+            std::basic_string_view<char> tmp(pattern.c_str() + counter - 1 - cur_str.size(), cur_str.size());
+            pattern_substrings.push_back(tmp);
             substr_end_position.push_back(counter - 1);
         }
     }
 
-    SymbolTree bor(pattern_substrings);
+    Trie trie(pattern_substrings);
     // pattern_positions[i] — the number of encountered in text pattern substrings
     // that begins in the text at i position.
-    std::vector<int32_t> pattern_positions;
-    FindAllEntries(bor, pattern_positions, substr_end_position);
+    std::vector<int32_t> pattern_positions = FindAllEntries(trie, substr_end_position);
 
     for (int i = 0; i < pattern_positions.size(); ++i) {
-        if (pattern_positions.at(i) == pattern_substrings.size() &&
-            pattern_positions.size() > i + counter - 1)
+        if (pattern_positions[i] == pattern_substrings.size() &&
+            pattern_positions.size() >= i + counter - 1)
             std::cout << i << ' ';
     }
 
